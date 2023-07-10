40b35aef223e2fe6ea19430df544e2bf855252d9
Fernando Espericueta <fernandostark18@gmail.com>
2023-07-09 23:17:25 -0600

    Basic app finished

--- soccer/backend/controllers/playerControllers.js
+++ soccer/backend/controllers/playerControllers.js
@@ -1,53 +1,51 @@
-import mongoose from "mongoose";
-import { PlayerSchema } from "../models/playerModel";
+import mongoose from 'mongoose';
+import { PlayerSchema } from '../models/playerModel';
 
-const Player = mongoose.model("Player", PlayerSchema);
+const Player = mongoose.model('Player', PlayerSchema);
 
 export const addNewPlayer = (req, res) => {
-  let newPlayer = new Player(req.body);
+    let newPlayer = new Player(req.body);
 
-  newPlayer.save().then((err, Player) => {
-    if (err) {
-      res.send(err);
-    }
-    res.json(Player);
-  });
+    newPlayer.save((err, Player) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(Player);
+    });
 };
 
 export const getPlayers = (req, res) => {
-  Player.find({}).then((err, Player) => {
-    if (err) {
-      res.send(err);
-    }
-    res.json(Player);
-  });
+    Player.find({},(err, Player) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(Player);
+    });
 };
 
 export const getPlayerWithID = (req, res) => {
-  Player.findById(req.params.PlayerId).then((err, Player) => {
-    if (err) {
-      res.send(err);
-    }
-    res.json(Player);
-  });
+    Player.findById(req.params.PlayerId,(err, Player) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(Player);
+    });
 };
 
-export const updatePlayer = (req, res) => {
-  Player.findOneAndUpdate({ _id: req.params.PlayerId }, req.body, {
-    new: true,
-  }).then((err, Player) => {
-    if (err) {
-      res.send(err);
-    }
-    res.json(Player);
-  });
+export const UpdatePlayer = (req, res) => {
+    Player.findOneAndUpdate({ _id: req.params.PlayerId}, req.body, {new: true}, (err, Player) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(Player);
+    });
 };
 
 export const deletePlayer = (req, res) => {
-  Player.deleteOne({ _id: req.params.PlayerId }).then((err, Player) => {
-    if (err) {
-      return res.status(404).send(err);
-    }
-    return res.status(200).json({ message: "Successfully deleted player" });
-  });
+    Player.remove({ _id: req.params.PlayerId},(err, Player) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json({ message: 'Successfully deleted player'});
+    });
 };
--- soccer/backend/index.js
+++ soccer/backend/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
-import bodyParser from "body-parser";
+import bodyparser from "body-parser";
 import cors from "cors";
-import routes from "./routes/soccerRuotes";
+import routes from "./routes/soccerRoutes";
 
 const app = express();
 const PORT = 4000;
@@ -15,8 +15,8 @@ mongoose.connect("mongodb://127.0.0.1:27017", {
 });
 
 // bodyparser setup
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
 
 // CORS setup
 app.use(cors());
